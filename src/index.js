f3baea940f37befed4f6dbb9ef547b4a595ddb71
Marcos Vazquez <mmvazquez364@gmail.com>
2026-06-02 21:32:24 -0400

    create todo form

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+"user strict";
+
 import "./styles.css";
+import {Task} from "./models/task.js";
+import {TodoForm} from "./doms/todo-form.js";
+
+const body = document.querySelector("body");
+const createTodoBtn = document.querySelector(".todo-btn");
+
+createTodoBtn.addEventListener('click', () => {
+    body.appendChild(TodoForm());
+});
